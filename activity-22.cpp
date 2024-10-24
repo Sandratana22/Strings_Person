b1bfe5ec927bf0c1b7618301547fa73accc97afc
Sandratana Malalaniaina <andrianjoharys06@gmail.com>
2024-10-23 18:38:21 -0700

    Add files via upload

---
+++ activity-22.cpp
@@ -0,0 +1,16 @@
+// activity-22.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include "strings_customer.h"
+
+using namespace std; 
+
+int main(){
+    string fullName = "Sandratana Andrianjohary";
+
+    personIdentity::string_person(fullName); 
+
+    return 0; 
+}
